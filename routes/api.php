671aa03607cf9a2522a6613ebda51198b2283e22
Prisecaru Emilia <prisecaru.emilia@gmail.com>
2022-05-16 17:50:24 +0300

    create category and subcategory tables

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\VisitorController;
 use App\Http\Controllers\Admin\ContactController;
 use App\Http\Controllers\Admin\SiteInfoController;
+use App\Http\Controllers\Admin\CategoryController;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
@@ -17,3 +18,6 @@ Route::post('/post-contact', [ContactController::class, 'PostContactDetails']);
 
 // site info
 Route::get('/all-site-info', [SiteInfoController::class, 'AllSiteInfo']);
+
+// all categories
+Route::get('/all-category', [CategoryController::class, 'AllCategory']);
