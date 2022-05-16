0b7b1993b8caf3c9183f9138a3b849af08df6c75
Prisecaru Emilia <prisecaru.emilia@gmail.com>
2022-05-16 14:45:27 +0300

    create site info API

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\VisitorController;
 use App\Http\Controllers\Admin\ContactController;
-
+use App\Http\Controllers\Admin\SiteInfoController;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
@@ -14,3 +14,6 @@ Route::get('/get-visitor', [VisitorController::class, 'GetVisitorDetails']);
 
 // contact
 Route::post('/post-contact', [ContactController::class, 'PostContactDetails']);
+
+// site info
+Route::get('/all-site-info', [SiteInfoController::class, 'AllSiteInfo']);
