0c4bb2cd8905651409f9ac8c2b29bd6443071ff7
Prisecaru Emilia <prisecaru.emilia@gmail.com>
2022-05-27 20:02:48 +0300

    create add to cart API - part 2

--- app/Http/Controllers/Admin/ProductCartController.php
+++ app/Http/Controllers/Admin/ProductCartController.php
@@ -5,10 +5,45 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\ProductCart;
+use App\Models\Product;
 
 class ProductCartController extends Controller
 {
     public function AddToCart(Request $request)
     {
+        $email = $request->input('email');
+        $size = $request->input('size');
+        $color = $request->input('color');
+        $quantity = $request->input('quantity');
+        $product_code = $request->input('product_code');
+
+        $productDetails = Product::where('product_code', $product_code)->get();
+
+        $price = $productDetails[0]['price'];
+        $special_price = $productDetails[0]['special_price'];
+
+        if ($special_price == '') {
+            $total_price = floatval($price) * intval($quantity);
+            $unit_price = $price;
+        } else {
+            $total_price = floatval($special_price) * intval($quantity);
+            $unit_price = $special_price;
+        }
+
+        $result = ProductCart::insert([
+
+            'email' => $email,
+            'product_image' => $productDetails[0]['image'],
+            'product_name' => $productDetails[0]['name'],
+            'product_code' => $productDetails[0]['product_code'],
+            'size' =>  $size,
+            'color' => $color,
+            'quantity' => $quantity,
+            'unit_price' => $unit_price,
+            'total_price' => $total_price,
+
+        ]);
+
+        return $result;
     }
 }
