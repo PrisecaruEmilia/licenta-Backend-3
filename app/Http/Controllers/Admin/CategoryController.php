1e1e33752a8faf0c0e525573614d216cd49cb0cf
Prisecaru Emilia <prisecaru.emilia@gmail.com>
2022-06-06 23:43:48 +0300

    add - store category - part 2

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Models\Subcategory;
+use Image;
 
 class CategoryController extends Controller
 {
@@ -40,5 +41,28 @@ class CategoryController extends Controller
 
     public function StoreCategory(Request $request)
     {
+        $request->validate([
+            'category_name' => 'required',
+        ], [
+            'category_name.required' => 'Câmpul nume categorie este obligatoriu'
+
+        ]);
+
+        $image = $request->file('category_image');
+        $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+        Image::make($image)->resize(500, 500)->save('upload/category/' . $name_gen);
+        $save_url = 'http://127.0.0.1:8000/upload/category/' . $name_gen;
+
+        Category::insert([
+            'category_name' => $request->category_name,
+            'category_image' => $save_url,
+        ]);
+
+        $notification = array(
+            'message' => 'Categoria a fost inserată cu succes!',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('all.category')->with($notification);
     }
 }
