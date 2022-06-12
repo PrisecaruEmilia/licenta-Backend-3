e5227239c977ba61333fa570ce780c7601f37d66
Prisecaru Emilia <prisecaru.emilia@gmail.com>
2022-06-12 23:37:26 +0300

    remove unnecessary compoenents - blade

--- resources/views/admin/body/header.blade.php
+++ resources/views/admin/body/header.blade.php
@@ -4,63 +4,10 @@
             <div class="mobile-toggle-menu"><i class='bx bx-menu'></i>
             </div>
             <div class="search-bar flex-grow-1">
-                <div class="position-relative search-bar-box">
-                    <input type="text" class="form-control search-control" placeholder="Type to search...">
-                    <span class="position-absolute top-50 search-show translate-middle-y"><i
-                            class='bx bx-search'></i></span>
-                    <span class="position-absolute top-50 search-close translate-middle-y"><i class='bx bx-x'></i></span>
-                </div>
+                {{-- search not implemented --}}
             </div>
             <div class="top-menu ms-auto">
-                <ul class="navbar-nav align-items-center">
-                    <li class="nav-item mobile-search-icon">
-                        <a class="nav-link" href="#"> <i class='bx bx-search'></i>
-                        </a>
-                    </li>
-                    <li class="nav-item dropdown dropdown-large">
-                        <a class="nav-link dropdown-toggle dropdown-toggle-nocaret" href="#" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false"> <i class='bx bx-category'></i>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-end">
-                            <div class="row row-cols-3 g-3 p-3">
-                                <div class="col text-center">
-                                    <div class="app-box mx-auto bg-gradient-cosmic text-white"><i
-                                            class='bx bx-group'></i>
-                                    </div>
-                                    <div class="app-title">Teams</div>
-                                </div>
-                                <div class="col text-center">
-                                    <div class="app-box mx-auto bg-gradient-burning text-white"><i
-                                            class='bx bx-atom'></i>
-                                    </div>
-                                    <div class="app-title">Projects</div>
-                                </div>
-                                <div class="col text-center">
-                                    <div class="app-box mx-auto bg-gradient-lush text-white"><i
-                                            class='bx bx-shield'></i>
-                                    </div>
-                                    <div class="app-title">Tasks</div>
-                                </div>
-                                <div class="col text-center">
-                                    <div class="app-box mx-auto bg-gradient-kyoto text-dark"><i
-                                            class='bx bx-notification'></i>
-                                    </div>
-                                    <div class="app-title">Feeds</div>
-                                </div>
-                                <div class="col text-center">
-                                    <div class="app-box mx-auto bg-gradient-blues text-dark"><i class='bx bx-file'></i>
-                                    </div>
-                                    <div class="app-title">Files</div>
-                                </div>
-                                <div class="col text-center">
-                                    <div class="app-box mx-auto bg-gradient-moonlit text-white"><i
-                                            class='bx bx-filter-alt'></i>
-                                    </div>
-                                    <div class="app-title">Alerts</div>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
+                <ul class="navbar-nav align-items-center d-none">
                     <li class="nav-item dropdown dropdown-large">
                         <a class="nav-link dropdown-toggle dropdown-toggle-nocaret position-relative" href="#"
                             role="button" data-bs-toggle="dropdown" aria-expanded="false"> <span
@@ -164,8 +111,8 @@
                                         <div class="notify bg-light-primary text-primary"><i class='bx bx-user-pin'></i>
                                         </div>
                                         <div class="flex-grow-1">
-                                            <h6 class="msg-name">New 24 authors<span
-                                                    class="msg-time float-end">1 day
+                                            <h6 class="msg-name">New 24 authors<span class="msg-time float-end">1
+                                                    day
                                                     ago</span></h6>
                                             <p class="msg-info">24 new authors joined last week</p>
                                         </div>
@@ -177,8 +124,8 @@
                                                 class='bx bx-door-open'></i>
                                         </div>
                                         <div class="flex-grow-1">
-                                            <h6 class="msg-name">Defense Alerts <span
-                                                    class="msg-time float-end">2 weeks
+                                            <h6 class="msg-name">Defense Alerts <span class="msg-time float-end">2
+                                                    weeks
                                                     ago</span></h6>
                                             <p class="msg-info">45% less alerts last 4 weeks</p>
                                         </div>
@@ -211,8 +158,8 @@
                                                 alt="user avatar">
                                         </div>
                                         <div class="flex-grow-1">
-                                            <h6 class="msg-name">Daisy Anderson <span
-                                                    class="msg-time float-end">5 sec
+                                            <h6 class="msg-name">Daisy Anderson <span class="msg-time float-end">5
+                                                    sec
                                                     ago</span></h6>
                                             <p class="msg-info">The standard chunk of lorem</p>
                                         </div>
@@ -390,14 +337,8 @@
                                 class="bx bx-cog"></i><span>Schimbă
                                 parola</span></a>
                     </li>
-                    <li><a class="dropdown-item" href="javascript:;"><i
+                    <li><a class="dropdown-item" href="{{ route('dashboard') }}"><i
                                 class='bx bx-home-circle'></i><span>Dashboard</span></a>
-                    </li>
-                    <li><a class="dropdown-item" href="javascript:;"><i
-                                class='bx bx-dollar-circle'></i><span>Earnings</span></a>
-                    </li>
-                    <li><a class="dropdown-item" href="javascript:;"><i
-                                class='bx bx-download'></i><span>Downloads</span></a>
                     </li>
                     <li>
                         <div class="dropdown-divider mb-0"></div>
