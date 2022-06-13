7e3abc54bf24a50b9cee909c66538a1cf931cbe7
Prisecaru Emilia <prisecaru.emilia@gmail.com>
2022-06-13 21:31:40 +0300

    change mail template for forgot password

--- resources/views/mail/forget.blade.php
+++ resources/views/mail/forget.blade.php
@@ -1,15 +1,26 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Forget Password</title>
-</head>
-<body>
-Bună <br>
 
- Change Your Password <a href="http://localhost:3000/reset/{{ $data }}">Click Here</a>
- <br>
- Pincode : {{ $data }}
+<head>
+    <title>Forget Password</title>
+</head>
+
+<body>
+    <h2>Splash Shop</h2>
+    <br>
+    Bună,
+    <br>
+
+    Ți-ai uitat parola?
+    <br>
+    Am primit un request de resetare a parolei pentru acest cont.
+    <br>
+    <br>
+    Pentru a reseta parola <a href="http://localhost:3000/reset/{{ $data }}">Apasă Aici</a>
+    <br>
+    Pincode : {{ $data }}
 
 
 </body>
+
 </html>
